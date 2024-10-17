465c479fe0ec1da4be44834838c7fe83bf96bbd4
Tyler Savery <tyler@theyoungastronauts.com>
2024-10-17 16:06:05 -0400

    set port to 10000

--- sp_cloud_server/start.sh
+++ sp_cloud_server/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i -e "s/\$INSERT_PORT/$PORT/g" /etc/nginx/nginx.conf
+# sed -i -e "s/\$INSERT_PORT/$PORT/g" /etc/nginx/nginx.conf
 
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
